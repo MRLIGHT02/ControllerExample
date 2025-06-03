1f5b36bcf583b81daf838d183d82a3ff6491cd0f
MRLIGHT02 <saifali42020@gmail.com>
2025-06-03 14:10:26 +0530

    #

---
+++ ControllerExample/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerExample.Controllers
+{
+    public class HomeController
+    {
+        [Route("hello")]
+        public string Method1()
+        {
+            return "Hello From Controller";
+        }
+        [Route(@"contact/num/{number:regex(^\d{{10}}$)}")]
+        public string Contact()
+        {
+            return "hello from contact ";
+        }
+
+    }
+
+}
